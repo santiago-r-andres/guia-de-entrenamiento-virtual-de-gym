82e5af1c0c654e5c7c87b12c1299e1c15990cd3a
Santiago Andres <santirafan@gmail.com>
2024-08-18 21:24:40 -0300

    Se incorpora metodo fetch, librerías y un sitio básico de e-commerce de packs de entrenamiento

--- js/main.js
+++ js/main.js
@@ -1,250 +1,275 @@
+const ejerciciosBrazos = ["flexiones", "tríceps", "bíceps"];
+const repeticionesBrazosFacil = [5, 10, 15];
+const repeticionesBrazosIntermedio = [10, 15, 20];
+const repeticionesBrazosDificil = [15, 20, 25];
 
-let ejerciciosBrazos = ['flexiones', 'tríceps', 'bíceps'];
-let repeticionesBrazosFacil = [5, 10, 15];
-let repeticionesBrazosIntermedio = [10, 15, 20];
-let repeticionesBrazosDificil = [15, 20, 25];
+const ejerciciosPiernas = ["prensa", "sentadillas", "estocadas"];
+const repeticionesPiernasFacil = [4, 8, 12];
+const repeticionesPiernasIntermedio = [8, 12, 16];
+const repeticionesPiernasDificil = [12, 16, 20];
 
-let ejerciciosPiernas = ['prensa', 'sentadillas', 'estocadas'];
-let repeticionesPiernasFacil = [4, 8, 12];
-let repeticionesPiernasIntermedio = [8, 12, 16];
-let repeticionesPiernasDificil = [12, 16, 20];
+
+const alumnos = JSON.parse(localStorage.getItem("alumnos")) || [];
+
 
 class Alumno {
-    static id = 0
+  static id = 0;
 
-    constructor (nombre, apellido, nivel) {
-        this.id = ++Alumno.id
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.nivelEjercicio = nivel
-    }
+  constructor(nombre, apellido, nivel) {
+    this.id = ++Alumno.id;
+    (this.nombre = nombre),
+    (this.apellido = apellido),
+    (this.nivelEjercicio = nivel);
+  }
 }
 
-let alumnos = JSON.parse(localStorage.getItem("alumnos")) || []
+
 
 
 // Se agrega al boton Registrarse el evento click y la función registrarAlumnos
-document.addEventListener("DOMContentLoaded", () => {
-    crearFormulario()
-    crearFormularioDias()
-    const botonRegistrarse = document.getElementById("botonRegistrar")
-    botonRegistrarse.addEventListener("click", registrarAlumnos)
-    const botonCreaRutina = document.getElementById("botonCreaRutina")
-botonCreaRutina.onclick = () => {
-    let diaDeLaSemana = document.getElementById("dia").value.toLowerCase()
-    let rutinaDelDia = determinarRutina(diaDeLaSemana)
-    document.getElementById("rutina-del-dia").textContent = "La rutina del día es " + rutinaDelDia
-    if (rutinaDelDia !== "día inválido") {
-        comenzarClase(rutinaDelDia)
-    } else {
-        document.getElementById("mensaje").textContent = "Día inválido"
-    }
-}
-})
 
+  crearFormulario();
+  crearFormularioDias();
+
+  const botonRegistrarse = document.getElementById("botonRegistrar");
+  botonRegistrarse.addEventListener("click", registrarAlumnos);
+
+  const botonCreaRutina = document.getElementById("botonCreaRutina");
+  botonCreaRutina.onclick = () => {
+    let diaDeLaSemana = document.getElementById("dias").value.toLowerCase();
+    let rutinaDelDia = determinarRutina(diaDeLaSemana);
+    document.getElementById("rutina-del-dia").textContent =
+      "La rutina del día es " + rutinaDelDia;
+    if (rutinaDelDia !== "día inválido") {
+      comenzarClase(rutinaDelDia);
+    } else {
+      document.getElementById("mensaje").textContent = "Día inválido";
+    }
+  }
 
 
 // CREACION DE FORMULARIO para sección Registrarse
-function crearFormulario () {
-    const form = document.createElement("form")
-    form.id = "formRegistro"
+function crearFormulario() {
+  const form = document.createElement("form");
+  form.id = "formRegistro";
 
+  // SE CREA labelNombre
+  const labelNombre = document.createElement("label");
+  labelNombre.for = "nombre";
+  labelNombre.innerText = "Nombre";
+  form.appendChild(labelNombre);
 
-// SE CREA labelNombre
-    const labelNombre = document.createElement("label")
-    labelNombre.for = "nombre"
-    labelNombre.innerText = "Nombre"
-    form.appendChild(labelNombre)
+  // SE CREA inputNombre
+  const inputNombre = document.createElement("input");
+  inputNombre.type = "text";
+  inputNombre.id = "nombre";
+  form.appendChild(inputNombre);
 
-// SE CREA inputNombre
-    const inputNombre = document.createElement("input")
-    inputNombre.type = "text"
-    inputNombre.id = "nombre"
-    form.appendChild(inputNombre)
+  // br
+  form.appendChild(document.createElement("br"));
 
-// br
-    form.appendChild(document.createElement("br"))
+  // SE CREA labelApellido
+  const labelApellido = document.createElement("label");
+  labelApellido.for = "apellido";
+  labelApellido.innerText = "Apellido";
+  form.appendChild(labelApellido);
 
-// SE CREA labelApellido
-    const labelApellido = document.createElement("label")
-    labelApellido.for = "apellido"
-    labelApellido.innerText = "Apellido"
-    form.appendChild(labelApellido)
+  // SE CREA inputApellido
+  const inputApellido = document.createElement("input");
+  inputApellido.type = "text";
+  inputApellido.id = "apellido";
+  form.appendChild(inputApellido);
 
-    // SE CREA inputApellido
-    const inputApellido = document.createElement("input")
-    inputApellido.type = "text"
-    inputApellido.id = "apellido"
-    form.appendChild(inputApellido)
+  // br
+  form.appendChild(document.createElement("br"));
 
-// br
-    form.appendChild(document.createElement("br"))
+  // SE CREA labelNivel
+  const labelNivel = document.createElement("label");
+  labelNivel.for = "nivel";
+  labelNivel.innerText = "Nivel de Entrenamiento";
+  form.appendChild(labelNivel);
 
-// SE CREA labelNivel
-    const labelNivel = document.createElement("label")
-    labelNivel.for = "nivel"
-    labelNivel.innerText = "Nivel de Entrenamiento"
-    form.appendChild(labelNivel)
+  // SE CREA selectNivel
+  const selectNivel = document.createElement("select");
+  selectNivel.id = "nivel";
+  selectNivel.required = true;
+  form.appendChild(selectNivel);
 
-// SE CREA selectNivel
-    const selectNivel = document.createElement("select")
-    selectNivel.id = "nivel"
-    selectNivel.required = ""
-    form.appendChild(selectNivel)
+  // SE CREAN opciones de Nivel de Entrenamiento
 
-// SE CREAN opciones de Nivel de Entrenamiento
+  const opciones = ["principiante", "intermedio", "difícil"];
+  for (const opcion of opciones) {
+    const option = document.createElement("option");
+    option.innerText = opcion.charAt(0).toUpperCase() + opcion.slice(1);
+    selectNivel.appendChild(option);
+  }
 
-    const opciones = ["principiante", "intermedio", "difícil"]
-    for (const opcion of opciones) {
-        const option = document.createElement("option")
-        option.value = opcion
-        option.textContent = opcion.charAt(0).toUpperCase() + opcion.slice(1)
-        selectNivel.appendChild(option)
-}
+  // br
+  form.appendChild(document.createElement("br"));
 
-// br
-form.appendChild(document.createElement("br"))
+  // SE CREA el botón REGISTRARSE
+  const boton = document.createElement("button");
+  boton.type = "button";
+  boton.id = "botonRegistrar";
+  boton.innerText = "REGISTRARSE";
+  boton.className = "btn";
+  form.appendChild(boton);
 
-// SE CREA el botón REGISTRARSE
-    const boton = document.createElement("button")
-    boton.type = "button"
-    boton.id = "botonRegistrar"
-    boton.innerText = "REGISTRARSE"
-    form.appendChild(boton)
+  // Se INCORPORA el elemento form al elemento padre section
 
-// Se INCORPORA el elemento form al elemento padre section
-    
-    const section = document.getElementById("container1")
-    section.appendChild(form)
+  const section = document.getElementById("container1");
+  section.appendChild(form);
 }
 
 // FORMULARIO PARA DETERMINAR RUTINA
 
-function crearFormularioDias () {
-    const form = document.createElement("form")
-    form.id = ""
+function crearFormularioDias() {
+  const form = document.createElement("form");
+  form.id = "";
 
-// SE CREA labelDia
-const labelDia = document.createElement("label")
-labelDia.for = "dia"
-labelDia.innerText = "Seleccione el Día de la Semana"
-form.appendChild(labelDia)
+  // SE CREA labelDia
+  const labelDia = document.createElement("label");
+  labelDia.for = "dia";
+  labelDia.innerText = "Seleccione el Día de la Semana";
+  form.appendChild(labelDia);
 
-// SE CREA selectDias
-const selectDias = document.createElement("select")
-selectDias.id = ""
-selectDias.required = ""
-form.appendChild(selectDias)
+  // SE CREA selectDias
+  const selectDias = document.createElement("select");
+  selectDias.id = "dias";
+  selectDias.required = true;
+  form.appendChild(selectDias);
 
-// SE CREAN opciones de Dias
+  // SE CREAN opciones de Dias
 
-const opciones = ["lunes", "martes", "miercoles", "jueves", "viernes"]
-for (const opcion of opciones) {
-    const option = document.createElement("option")
-    option.id = "dia"
-    option.value = opcion
-    option.textContent = opcion.charAt(0).toUpperCase() + opcion.slice(1)
-    selectDias.appendChild(option)
+  let dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+  for (const dia of dias) {
+    const opcion = document.createElement("option");
+    opcion.innerText = dia;
+    selectDias.appendChild(opcion);
+  }
+
+  // br
+  form.appendChild(document.createElement("br"));
+
+  // SE CREA el botón CREAR RUTINA
+  const boton = document.createElement("button");
+  boton.type = "button";
+  boton.id = "botonCreaRutina";
+  boton.className = "btn";
+  boton.innerText = "CREAR RUTINA";
+  form.appendChild(boton);
+
+  const section2 = document.getElementById("container2");
+  section2.appendChild(form);
 }
-
-// br
-form.appendChild(document.createElement("br"))
-
-// SE CREA el botón CREAR RUTINA
-const boton = document.createElement("button")
-boton.type = "button"
-boton.id = "botonCreaRutina"
-boton.innerText = "CREAR RUTINA"
-form.appendChild(boton)
-
-
-const section2 = document.getElementById("container2")
-section2.appendChild(form)
-}
-
 
 function registrarAlumnos() {
+  let cargarNombre = document.getElementById("nombre").value;
+  let cargarApellido = document.getElementById("apellido").value;
+  let cargarNivel = document.getElementById("nivel").value.toLowerCase();
 
-    let cargarNombre = document.getElementById("nombre").value
-    let cargarApellido = document.getElementById("apellido").value
-    // let cargarEdad = document.getElementById("edad").value
-    // let cargarPeso = document.getElementById("peso").value
-    let cargarNivel = document.getElementById("nivel").value
+  const alumno = new Alumno(cargarNombre, cargarApellido, cargarNivel);
 
-    const alumno = new Alumno (cargarNombre, cargarApellido, cargarNivel)
+  alumnos.push(alumno);
 
-    alumnos.push(alumno)
-
-    localStorage.setItem("alumnos", JSON.stringify(alumnos))
-    document.getElementById("formRegistro").reset()
+  localStorage.setItem("alumnos", JSON.stringify(alumnos));
+  document.getElementById("formRegistro").reset();
 }
-
-
 
 function determinarRutina(diaDeLaSemana) {
-    switch(diaDeLaSemana){
-        case 'lunes':
-        case 'viernes':
-            return 'piernas';
-        case 'martes':
-        case 'jueves':
-            return 'brazos';
-        case 'miercoles':
-            return 'día de descanso';
-        default:
-            return 'día inválido';
-    }
+  switch (diaDeLaSemana) {
+    case "lunes":
+    case "viernes":
+      return "piernas";
+    case "martes":
+    case "jueves":
+      return "brazos";
+    case "miercoles":
+      return "día de descanso";
+    default:
+      return "día inválido";
+  }
 }
-
 
 function comenzarClase(rutinaDelDia) {
-    let mensaje = ""
-    alumnos.forEach ((alumno) => {
-        let tipoAlumno = alumno.nivelEjercicio
-        mensaje += "El alumno " + alumno.nombre + " " + alumno.apellido + " es de nivel: " + tipoAlumno
-        switch (rutinaDelDia) {
-            case 'brazos':
-                switch(tipoAlumno) {
-                    case 'principiante':
-                        mensaje += ejecutarRutina(alumno, ejerciciosBrazos, repeticionesBrazosFacil);
-                        break;
-                    case 'intermedio':
-                        mensaje += ejecutarRutina(alumno, ejerciciosBrazos, repeticionesBrazosIntermedio)
-                        break;
-                    case 'difícil':
-                        mensaje += ejecutarRutina(alumno, ejerciciosBrazos, repeticionesBrazosDificil)
-                        break;
-                    }
-                    break;
-            case 'piernas':
-                switch(tipoAlumno) {
-                    case 'principiante':
-                        mensaje += ejecutarRutina(alumno, ejerciciosPiernas, repeticionesPiernasFacil);
-                        break;
-                    case 'intermedio':
-                        mensaje += ejecutarRutina(alumno, ejerciciosPiernas, repeticionesPiernasIntermedio)
-                        break;
-                    case 'difícil':
-                        mensaje += ejecutarRutina(alumno, ejerciciosPiernas, repeticionesPiernasDificil)
-                        break;
-                    }
-                    break;
-                    default:
-                        mensaje += "Hoy es tu día de descanso! Disfruta tu día libre!!"
-}
-        })
-        document.getElementById("mensaje").textContent = mensaje
+  alumnos.forEach((alumno) => {
+    let mensaje = document.createElement("h3")
+    const section3 = document.getElementById("container3");
+    section3.appendChild(mensaje)
+    let tipoAlumno = alumno.nivelEjercicio;
+    mensaje.innerText = `El alumno ${alumno.nombre} ${alumno.apellido} es de nivel: ` + tipoAlumno;
+    switch (rutinaDelDia) {
+      case "brazos":
+        switch (tipoAlumno) {
+          case "principiante":
+            ejecutarRutina(
+              alumno,
+              ejerciciosBrazos,
+              repeticionesBrazosFacil
+            );
+            break;
+          case "intermedio": 
+            ejecutarRutina(
+              alumno,
+              ejerciciosBrazos,
+              repeticionesBrazosIntermedio
+            );
+            break;
+          case "difícil": 
+            ejecutarRutina(
+              alumno,
+              ejerciciosBrazos,
+              repeticionesBrazosDificil
+            );
+            break;
+        }
+        break;
+      case "piernas":
+        switch (tipoAlumno) {
+          case "principiante":
+            ejecutarRutina(
+              alumno,
+              ejerciciosPiernas,
+              repeticionesPiernasFacil
+            );
+            break;
+          case "intermedio":
+            ejecutarRutina(
+              alumno,
+              ejerciciosPiernas,
+              repeticionesPiernasIntermedio
+            );
+            break;
+          case "difícil":
+            ejecutarRutina(
+              alumno,
+              ejerciciosPiernas,
+              repeticionesPiernasDificil
+            );
+            break;
+        }
+        break;
+      default:
+        mensaje.innerText = `${alumno.nombre} ${alumno.apellido} hoy es tu día de descanso! Disfruta tu día libre!!`
+        break;
     }
+  });
+}
 
 function ejecutarRutina(alumno, ejercicios, repeticiones) {
-        let mensaje = "El alumno " + alumno.nombre + alumno.apellido + " debe realizar la siguiente rutina: "
-            for(let c = 0; c < ejercicios.length; c++) {
-            mensaje += "Ejercicio: " + ejercicios[c] + "\n" 
-            mensaje += "Cantidad de repeticiones: " + repeticiones[c] + "\n"
-        }
-        mensaje += "Mucha suerte y que la fuerza te acompañe!\n"
-        return mensaje
+  let mensaje = document.createElement("h4")
+  mensaje.innerText = `El alumno ${alumno.nombre} ${alumno.apellido} debe realizar la siguiente rutina: `
+  const section3 = document.getElementById("container3");
+    section3.appendChild(mensaje);
+  
+    for (let c = 0; c < ejercicios.length; c++) {
+    let parrafo1 = document.createElement("p");
+    parrafo1.innerText = "Ejercicio: " + ejercicios[c] + "\n";
+    let parrafo2 = document.createElement("p");
+    parrafo2.innerText = "Cantidad de repeticiones: " + repeticiones[c] + "\n";
+    section3.appendChild(parrafo1);
+    section3.appendChild(parrafo2);
+  }
 }
-
 
 
---
+++ js/packs.js
@@ -0,0 +1,201 @@
+    const file = '../db/data.json'
+    const productsContainer = document.getElementById('packs')
+    const cartContainer = document.getElementById('carrito')
+    const cantidadPacks = document.querySelector(".count-packs")
+    const checkoutButton = document.getElementById('checkout')
+    let cart = [] 
+
+
+    class Pack {
+        constructor(clase, precio, id) {
+            // this.imagen = imagen;
+            this.clase = clase;
+            this.precio = precio;
+            this.id = id;
+            this.cantidad = 1;
+            this.subtotal = 0;
+        }
+        obtenerTotal() {
+            this.subtotal = this.precio * this.cantidad;
+        } 
+    }
+
+
+    renderizarPacks()
+    loadCarritoLS()
+    actualizarCarrito()
+    
+
+// Función para realizar petición a la BD data.json
+    async function realizarPeticion(datos) {
+        try {
+            const response = await fetch(datos);
+            if (!response.ok) throw new Error(`Error en la petición: ${response.status} ${response.statusText}`);
+            const products = await response.json();
+            return products
+        } catch (error) {
+            console.error('Error en la petición del server', error);
+        } finally {
+            console.log('Petición completada');
+        }
+    }
+    
+
+
+// Función para renderizar packs de entrenamiento
+    async function renderizarPacks() {
+        const products = await realizarPeticion(file)
+        products.forEach(product => {
+            const contenedor = document.createElement("div")
+            contenedor.className = "product"
+            contenedor.innerHTML += `<h3>${product.clase}</h3>
+                                    <p>Precio: $${product.precio}</p>
+                                    <button id=${product.id} class="agregar-carrito">Agregar al Carrito</button>`
+                                
+            productsContainer.appendChild(contenedor)
+                            })
+
+        const addButtons = document.querySelectorAll(".agregar-carrito")
+            addButtons.forEach(button => {
+            button.addEventListener("click", agregarProducto)       
+                    })
+    }
+    
+
+
+function loadCarritoLS() {
+    cart = JSON.parse(localStorage.getItem('cart')) || []
+}
+
+function agregarProducto(e) {
+    if(e.target.classList.contains("agregar-carrito")) {
+        const packAdded = e.target.parentElement
+        leerDatosProducto(packAdded)
+        Toastify({
+
+            text: "Pack Agregado",
+            
+            duration: 2000
+            
+            }).showToast();
+    }
+}
+
+function leerDatosProducto(pack) {
+    let clase, precio, id
+    clase = pack.querySelector("h3").textContent
+    precio = Number(pack.querySelector("p").textContent.replace(/[^0-9]/g, ""))
+    id = parseInt(pack.querySelector("button").getAttribute("id"))
+
+    
+
+    const dataPack = new Pack(clase, precio, id)
+    dataPack.obtenerTotal()
+
+    agregarAlCarrito(dataPack)
+}
+
+
+// Función para agregar productos al carrito
+    function agregarAlCarrito(addPack) {
+        const packEnCarrito = cart.some((p) => p.id === addPack.id)
+        if (packEnCarrito) {
+            const packs = cart.map((pack) => {
+                if (pack.id === addPack.id) {
+                    pack.cantidad++
+                    pack.subtotal = pack.precio * pack.cantidad
+                    return pack
+                } else {
+                    return pack
+                }
+            })
+        
+            cart = packs
+        } else {
+            cart.push(addPack)
+        }
+        guardarCarrito()
+        actualizarCarrito()
+}
+
+
+// Función que actualiza el contenido del carrito
+    function actualizarCarrito() {
+        limpiarHTML()
+        
+        cart.forEach((producto) => {
+            const {clase, precio, cantidad, subtotal, id} = producto;
+
+            const div = document.createElement("div")
+                div.classList.add("cart-item")
+                div.innerHTML = `
+                     <h3>${clase}</h3>
+                     <P>Precio: $${precio}</P>
+                     <P>Cantidad: ${cantidad}</P>
+                    <P>Subtotal: $${subtotal}</P>
+                    <button id=${id} class="remove-btn"> X </button>
+        `
+        cartContainer.appendChild(div)
+    })
+
+    const removeButtons = document.querySelectorAll(".remove-btn")
+    removeButtons.forEach(button => {
+    button.addEventListener("click", eliminarDelCarrito)
+})
+mostrarCantidadProductos()
+mostrarTotal()
+    }
+
+// Función para limpiar el HTML
+function limpiarHTML() {
+    while (cartContainer.firstChild) {
+        cartContainer.removeChild(cartContainer.firstChild)
+    }
+}
+
+// Función para mostrar la cantidad de productos
+
+function mostrarCantidadProductos() {
+    let contarProductos
+
+    if (cart.length > 0) {
+        contarProductos = cart.reduce((cantidad, producto) => cantidad + producto.cantidad, 0)
+        console.log(contarProductos) 
+        cantidadPacks.innerText = `${contarProductos}`
+    }
+}
+
+
+// Función que elimina productos del carrito
+    function eliminarDelCarrito(e) {
+        if (e.target.classList.contains("remove-btn")) {
+            const productoId = parseInt(e.target.getAttribute("id"))
+            cart = cart.filter(p => p.id !== productoId)
+            guardarCarrito()
+            console.log(cart)
+            actualizarCarrito() 
+        }
+    }
+
+// Función que guarda en localStorage los productos agregados al carrito
+    function guardarCarrito() {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
+
+    // Función para mostrar el total del carrito
+function mostrarTotal() {
+    const totalElement = document.getElementById('total');
+    const total = cart.reduce((acc, producto) => acc + producto.subtotal, 0);
+    totalElement.innerHTML = `Total a Pagar: $ ${total}`
+}
+
+    checkoutButton.addEventListener('click', () => {
+        Swal.fire({
+            title: "Compra Existosa!",
+            text: "Gracias por elegirnos!",
+            icon: "success"
+          });
+        cart = []
+        guardarCarrito()
+        actualizarCarrito()
+    });
